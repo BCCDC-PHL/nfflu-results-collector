5014d913d4ec08a13e9990d68d6df0ed5839273c
John Palmer <john.palmer@bccdc.ca>
2026-04-21 11:04:22 -0700

    made nextclade_rc accessible

--- nfflu_results_collector/__init__.py
+++ nfflu_results_collector/__init__.py
@@ -1,3 +1,4 @@
 from nfflu_results_collector.collector import Nfflu_Results_Collector
+from nfflu_results_collector.nextclade import Nextclade_Results_Collector
 
-__all__ = ['Nfflu_Results_Collector']
+__all__ = ['Nfflu_Results_Collector', 'Nextclade_Results_Collector']
